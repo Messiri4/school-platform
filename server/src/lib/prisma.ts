f84df4b4b325cea477c338e7c8c522fdf3f30443
Messiri4 <messirimiracle@gmail.com>
2026-07-19 01:26:07 -0700

    add prisma generate to dockerfile

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
-// @ts-ignore
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-const prisma = new PrismaClient({ adapter } as any)
+// @ts-ignore
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
